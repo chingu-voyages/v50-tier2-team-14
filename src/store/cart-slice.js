b8134f95fccca9771a049b0650a651ccca8160e7
Olga Yudkin <cvtqx@protonmail.com>
2024-07-22 11:50:05 -0400

    added cart-slice.js and addtocart reducer

---
+++ src/store/cart-slice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        itemsList: [],
+        totalQuantity: 0,
+        showCart: false
+    },
+    reducers: {
+        addToCart(state, action) {
+            const newItem = action.payload;
+            //check if item is already in cart
+            const existingItem = state.itemsList.find(item => item.id === newItem.id);
+            if (existingItem) {
+                existingItem.count++;
+                existingItem.price += newItem.price;
+            } else {
+                state.itemsList.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.name
+                })
+            }
+        },
+        removeFromCart() { },
+        setShowCart(state) {
+            state.showCart = true
+        }
+    }
+})
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
