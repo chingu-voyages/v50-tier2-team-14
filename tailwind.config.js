4040e31846ff617febac10f4f53ff1cbe17bc1ac
Alvarado08 <jorgealvarado0238@gmail.com>
2024-07-23 22:07:45 -0500

    removed tailwind.config extends block

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  theme: {
-    extend: {
-      colors: {
-        "darker-brown": "#a84e1c",
-        "lighter-brown": "#c27347",
-        "peachy-brown": "#ed9564",
-        "piggy-pink": "#fcb8c4",
-        "rabbit-body-nude": "#ffe1cf",
-      },
-    },
-  },
+  theme: {},
   plugins: [require("daisyui")],
   // daisyUI config (optional - here are the default values)
   daisyui: {
