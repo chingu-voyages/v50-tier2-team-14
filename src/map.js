8b28a338a0a33045c572fbb3fc410112a2500051
Shimin <natkuma@outloook.com>
2024-07-26 12:46:34 -0400

    Implemented custom markers on the map - cutlery logo/ red_marker

--- src/map.js
+++ src/map.js
@@ -1,22 +1,29 @@
 import React from 'react'
-import "./map.css"
 import "leaflet/dist/leaflet.css"
+import redMarker from"./asset/images/red_marker.png"
+import cutlery from"./asset/images/cutlery.png"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import { Icon } from "leaflet"
 
 
 function Map() {
 
   const position = [51.505, -0.09]
 
+  const customIcon = new Icon({
+    iconUrl: cutlery,
+    iconSize: [40, 40]
+  })
+
 
   return (
-    <div>
-    <MapContainer center={position} zoom={13}>      
+    <div className="my-10 flex items-center justify-center">
+    <MapContainer className="h-80 w-4/5" center={position} zoom={13}>      
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={position}>
+    <Marker position={position} icon={customIcon}>
       <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
       </Popup>
