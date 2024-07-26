6a2fbb27589a533dcf299f903f4b2eed98cb2f63
Olga Yudkin <cvtqx@protonmail.com>
2024-07-25 21:52:38 -0400

    added navbar component to app and removed old content from app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <p className="text-5xl flex justify-center font-extrabold text-orange-500 mt-10">
-    //       Hungry Rabbit
-    //     </p>
-    //   </header>
-    //   <div className="p-5 space-y-4">
-    //     <h2 className="text-2xl font-semibold">
-    //       Daisy UI Custom Theme Testing
-    //     </h2>
-    //     <div className="flex items-center gap-4">
-    //       <button className="btn btn-primary">Primary</button>
-    //       <button className="btn btn-secondary">Secondary</button>
-    //       <button className="btn btn-accent">Accent</button>
-    //       <button className="btn btn-neutral">Neutral</button>
-    //       <button className="btn btn-base-100">Base</button>
-    //     </div>
-    //   </div>
-    // </div>
+  return ( 
     <Navbar />
   );
 }
