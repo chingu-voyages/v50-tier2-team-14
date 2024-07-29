af0bff69167fb8455cab6b26ff3561502b9462d7
Shimin <natkuma@outloook.com>
2024-07-29 02:10:48 -0400

    Installed React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return ( 
+  <>
+    <BrowserRouter>
     <Navbar />
+    <Routes>
+    </Routes>
+    </BrowserRouter>
+  </>
   );
 }
 
