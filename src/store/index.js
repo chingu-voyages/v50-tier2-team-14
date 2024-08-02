2caa0734a8b514ae6ba82871b8050bdd98855297
Olga Yudkin <cvtqx@protonmail.com>
2024-08-02 14:47:58 -0400

    added menu fetching logic to menuslice and updated menu component

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cart-slice";
+import menuSlice from "./menu-slice";
 
 const store = configureStore({
     reducer: {
         cart: cartSlice.reducer,
+        menu: menuSlice.reducer
     }
 });
 
