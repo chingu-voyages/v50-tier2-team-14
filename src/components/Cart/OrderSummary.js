f1221173a3b2420346f3e6d4bd2698b7c0a2eebd
Olga Yudkin <cvtqx@protonmail.com>
2024-08-05 12:54:20 -0400

    tip section draft done

--- src/components/Cart/OrderSummary.js
+++ src/components/Cart/OrderSummary.js
@@ -1,7 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from '../../store/cart-slice';
+import { useState } from "react";
 
 const OrderSummary = () => {
+    const [tipAmount, setTipAmount] = useState('');
+
     const cart = useSelector(state => state.cart);
 
     const dispatch = useDispatch();
@@ -10,9 +13,56 @@ const OrderSummary = () => {
         return accumulator + item.totalPrice;
       }, 0);
     
+    const cartTotalPriceWithTip = cartTotalPrice + cartTotalPrice * parseInt(tipAmount) / 100;
+    
       const handleCheckoutClick = () => {
         dispatch(cartActions.setShowCheckout());
       };
+    
+    const handleTipSelection = (amount) => {
+        setTipAmount(amount);
+
+    }
+ 
+    //TO DO: add custom tip input
+    const TipSuggestions = () => {
+    return (
+      <div>
+        <h3 className='text-lg font-bold mb-4'>Select a Tip Percentage</h3>
+        <div className='flex space-x-4 mb-4'>
+          <button
+            className='btn btn-primary'
+            onClick={() => handleTipSelection(15)}>
+            15%
+          </button>
+          <button
+            className='btn btn-primary'
+            onClick={() => handleTipSelection(18)}>
+            18%
+          </button>
+          <button
+            className='btn btn-primary'
+            onClick={() => handleTipSelection(22)}>
+            22%
+          </button>
+        </div>
+      </div>
+    );
+    }
+   //TODO: make this stand out 
+    const OrderTotal = () => {
+        return (
+          <div className='mb-4'>
+            <h4 className='mt-3 mb-5'>
+              Your total is{' '}
+              <span className='font-bold ml-10'>{cartTotalPriceWithTip}</span>
+            </h4>
+            <button className='btn hover:bg-accent-800 hover:text-white text-black rounded-full w-full py-3'>
+              Place order
+            </button>
+          </div>
+        );
+    }
     
   return (
     <div className='lg:w-1/4 w-full bg-gray-100 p-6 rounded-lg shadow'>
@@ -24,14 +74,24 @@ const OrderSummary = () => {
         Shipping <span className='font-bold ml-10'> FREE</span>
       </div>
       <hr className='my-4 border-t-2 border-gray-300' />
-      <div className='py-2 text-lg font-semibold mb-4'>
-        Total price: {cartTotalPrice} $
-      </div>
-      <button
-        className='btn hover:bg-accent-800 hover:text-white text-black rounded-full w-full py-3'
-        onClick={handleCheckoutClick}>
-        Proceed to checkout
-      </button>
+      {!cart.showCheckout && (
+        <>
+          <div className='py-2 text-lg font-semibold mb-4'>
+            Total price: {cartTotalPrice} $
+          </div>
+          <button
+            className='btn hover:bg-accent-800 hover:text-white text-black rounded-full w-full py-3'
+            onClick={handleCheckoutClick}>
+            Proceed to checkout
+          </button>
+        </>
+      )}
+      {cart.showCheckout && (
+        <>
+                  <TipSuggestions />
+                  <OrderTotal />
+        </>
+      )}
     </div>
   );
 };
--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import OrderSummary from '../Cart/OrderSummary'
+
+
 
 const Checkout = () => {
     const cart = useSelector(state => state.cart)
     console.log(cart)
   return (
     <div className='container mx-auto my-10 px-4 card shadow-xl'>
-      <h2 className='card-title justify-center pt-10'>Check out</h2>
-      <div className='card-body flex flex-col lg:flex-row gap-4'>
-        
-      </div>
+          <OrderSummary />
+          
     </div>
   );
 }
